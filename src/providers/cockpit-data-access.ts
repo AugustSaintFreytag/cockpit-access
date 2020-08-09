71c025289d1b91ba3c536f4a0d338aab2d275719
August Saint Freytag <night@apricummedia.com>
2020-08-09 10:31:36 +0200

    Reformat cockpit data access, update indentation

--- src/providers/cockpit-data-access.ts
+++ src/providers/cockpit-data-access.ts
@@ -10,73 +10,73 @@ import { Dictionary, URL } from "@/library/types"
 
 type AnyRequestObject = Dictionary<any>
 
-	// Options
+// Options
+
+function defaultOptions(): AnyRequestObject {
+	return {
+		populate: 3
+	}
+}
+
+// Typed Fetch
+
+export async function recordsInCollection(collection: string, requestOptions?: CockpitRequestOptions): Promise<CockpitCollectionResponse> {
+	const route = `api/collections/get/${collection}`
+	return await data(route, requestOptions)
+}
+
+export async function singletonRecord(singleton: string, requestOptions?: CockpitRequestOptions): Promise<CockpitSingletonResponse> {
+	const route = `api/singletons/get/${singleton}`
+	return await data(route, requestOptions)
+}
+
+// General Fetch
+
+export async function data(route: string, requestOptions?: CockpitRequestOptions): Promise<AnyCockpitResponse> {
+	const url = preparedUrl(route)
+
+	try {
+		const options = preparedOptions(defaultOptions(), requestOptions || {})
+		const serverResponse = await axios.post(url, options)
+
+		return serverResponse.data as CockpitCollectionResponse
+	} catch (err) {
+		throw new CockpitError(`Could not get response from cockpit for url '${obfuscatedUrl(url)}'. ${err}`)
+	}
+}
+
+function obfuscatedUrl(url: URL): URL {
+	return url.replace(/(token=)([a-fA-F0-9]+)(&*)/, "$1redacted$3")
+}
+
+// Preparation
+
+function preparedUrl(route: string): URL {
+	const currentAddress = address()
+
+	const protocol = currentAddress.protocol()
+	const host = currentAddress.host()
+	const token = currentAddress.token()
 	
-	function defaultOptions(): AnyRequestObject {
-		return {
-			populate: 3
-		}
-	}
+	return `${protocol}://${path.join(host, route)}?token=${token}`
+}
 
-	// Typed Fetch
-
-	export async function recordsInCollection(collection: string, requestOptions?: CockpitRequestOptions): Promise<CockpitCollectionResponse> {
-		const route = `api/collections/get/${collection}`
-		return await data(route, requestOptions)
-	}
-
-	export async function singletonRecord(singleton: string, requestOptions?: CockpitRequestOptions): Promise<CockpitSingletonResponse> {
-		const route = `api/singletons/get/${singleton}`
-		return await data(route, requestOptions)
-	}
-
-	// General Fetch
-
-	export async function data(route: string, requestOptions?: CockpitRequestOptions): Promise<AnyCockpitResponse> {
-		const url = preparedUrl(route)
-
-		try {
-			const options = preparedOptions(defaultOptions(), requestOptions || {})
-			const serverResponse = await axios.post(url, options)
-
-			return serverResponse.data as CockpitCollectionResponse
-		} catch (err) {
-			throw new CockpitError(`Could not get response from cockpit for url '${obfuscatedUrl(url)}'. ${err}`)
-		}
-	}
-
-	function obfuscatedUrl(url: URL): URL {
-		return url.replace(/(token=)([a-fA-F0-9]+)(&*)/, "$1redacted$3")
-	}
-
-	// Preparation
-
-	function preparedUrl(route: string): URL {
-		const currentAddress = address()
-
-		const protocol = currentAddress.protocol()
-		const host = currentAddress.host()
-		const token = currentAddress.token()
+function preparedOptions(baseOptions: AnyRequestObject, requestOptions: AnyRequestObject): AnyRequestObject {
+	const mergedOptions = baseOptions
+	
+	for (const key in requestOptions) {
+		const value = requestOptions[key]
 		
-		return `${protocol}://${path.join(host, route)}?token=${token}`
-	}
-
-	function preparedOptions(baseOptions: AnyRequestObject, requestOptions: AnyRequestObject): AnyRequestObject {
-		const mergedOptions = baseOptions
-		
-		for (const key in requestOptions) {
-			const value = requestOptions[key]
-			
-			if (typeof value === "object" && value !== null) {
-				mergedOptions[key] = preparedOptions(mergedOptions[key] || {}, value)
-			} else {
-				mergedOptions[key] = value
-			}
+		if (typeof value === "object" && value !== null) {
+			mergedOptions[key] = preparedOptions(mergedOptions[key] || {}, value)
+		} else {
+			mergedOptions[key] = value
 		}
-
-		return mergedOptions
 	}
 
-	function address(): Connection.Address {
-		return AddressRegister.defaultAddress()
-	}
+	return mergedOptions
+}
+
+function address(): Connection.Address {
+	return AddressRegister.defaultAddress()
+}

c937436347e9f8d16dafeec3593b143480aac070
August Saint Freytag <night@apricummedia.com>
2020-08-10 09:38:25 +0200

    Update project index file, destructure and re-export packed modules
    Direct re-export with renamed whole-module import "* as" is replaced with a
    destructured import and exporting as a fixed module.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import { cockpitAsset, cockpitImage } from "@/functions/cockpit-asset-path-form"
+import { data, recordsInCollection, singletonRecord } from "@/functions/cockpit-data-access"
+import { dateFromString, dateFromTimestamp, stringFromDate } from "@/functions/cockpit-date-conversion"
+import { joinedParameters } from "@/functions/query-parameter-form"
+
 export { addressRegister } from "@/configuration/address-register"
-export * from "@/functions/cockpit-asset-path-form"
-export * from "@/functions/cockpit-data-access"
-export * from "@/functions/cockpit-date-conversion"
-export * from "@/functions/query-parameter-form"
 export { CockpitError } from "@/library/cockpit-error"
 export { Context, Environment } from "@/library/connection"
 export type { Address, APIToken, Protocol } from "@/library/connection"
@@ -12,3 +13,8 @@ export { CockpitImageOptions, CockpitImageRequest } from "@/models/cockpit-image
 export { CockpitMetaData } from "@/models/cockpit-meta-data"
 export { CockpitRequestOptions } from "@/models/cockpit-request-options"
 export { AnyCockpitResponse, CockpitCollectionResponse, CockpitImageResponse, CockpitSingletonResponse } from "@/models/cockpit-response"
+
+export const CockpitAssetPathForm = { cockpitAsset, cockpitImage }
+export const CockpitDataAccess = { recordsInCollection, singletonRecord, data }
+export const CockpitDateConversion = { dateFromString, dateFromTimestamp, stringFromDate }
+export const QueryParameterForm = { joinedParameters }

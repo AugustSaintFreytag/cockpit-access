a084d0426380988ca05cd39bbbbf83760fd241ad
August Saint Freytag <night@apricummedia.com>
2021-02-03 21:37:34 +0100

    Rewrite index file, use direct module re-exports

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,13 @@
-import { cockpitAsset, cockpitImage } from "~/functions/cockpit-asset-path-form"
-import { data, recordsInCollection, singletonRecord } from "~/functions/cockpit-data-access"
-import { dateFromString, dateFromTimestamp, stringFromDate } from "~/functions/cockpit-date-conversion"
-import { joinedParameters } from "~/functions/query-parameter-form"
-
 export { addressRegister } from "~/configuration/address-register"
+export * as CockpitAssetPathForm from "~/functions/cockpit-asset-path-form"
+export * as CockpitDataAccess from "~/functions/cockpit-data-access"
+export * as CockpitDateConversion from "~/functions/cockpit-date-conversion"
+export * as QueryParameterForm from "~/functions/query-parameter-form"
 export { CockpitError } from "~/library/cockpit-error"
-export type { Context, Environment } from "~/library/connection"
-export type { Address, APIToken, Protocol } from "~/library/connection"
+export type { Address, APIToken, Context, Environment, Protocol } from "~/library/connection"
 export { CockpitEntry } from "~/models/cockpit-entry"
 export { CockpitFieldDefinition } from "~/models/cockpit-field-definition"
 export { CockpitImageOptions, CockpitImageRequest } from "~/models/cockpit-image-request"
 export { CockpitMetaData } from "~/models/cockpit-meta-data"
 export { CockpitRequestOptions } from "~/models/cockpit-request-options"
 export { AnyCockpitResponse, CockpitCollectionResponse, CockpitImageResponse, CockpitSingletonResponse } from "~/models/cockpit-response"
-
-export const CockpitAssetPathForm = { cockpitAsset, cockpitImage }
-export const CockpitDataAccess = { recordsInCollection, singletonRecord, data }
-export const CockpitDateConversion = { dateFromString, dateFromTimestamp, stringFromDate }
-export const QueryParameterForm = { joinedParameters }

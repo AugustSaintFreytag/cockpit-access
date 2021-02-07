a5859fc85bdc0336c5e9fc6b0d323fbe9f697aea
August Saint Freytag <night@apricummedia.com>
2021-02-07 23:09:29 +0100

    Rewrite re-export for namespaced Cockpit image opts. in index file

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ export { Context, Environment } from "~/library/connection"
 export type { Address, APIToken, Protocol } from "~/library/connection"
 export type { CockpitEntry } from "~/models/cockpit-entry"
 export type { CockpitFieldDefinition } from "~/models/cockpit-field-definition"
-export * as CockpitImageOptions from "~/models/cockpit-image-options"
 export { CockpitImageRequest } from "~/models/cockpit-image-request"
 export { CockpitMetaData } from "~/models/cockpit-meta-data"
 export type { CockpitRequestOptions } from "~/models/cockpit-request-options"
@@ -13,7 +12,9 @@ import * as _CockpitAssetPathForm from "~/functions/cockpit-asset-path-form"
 import * as _CockpitDataAccess from "~/functions/cockpit-data-access"
 import * as _CockpitDateConversion from "~/functions/cockpit-date-conversion"
 import * as _QueryParameterForm from "~/functions/query-parameter-form"
+import * as _CockpitImageOptions from "~/models/cockpit-image-options"
 
+export const CockpitImageOptions = { ... _CockpitImageOptions }
 export const CockpitAssetPathForm = { ..._CockpitAssetPathForm }
 export const CockpitDataAccess = { ... _CockpitDataAccess }
 export const CockpitDateConversion = { ... _CockpitDateConversion }

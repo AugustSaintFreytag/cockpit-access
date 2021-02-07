ad5217608282dc1a7ace25558cd7415ce7281ed4
August Saint Freytag <night@apricummedia.com>
2021-02-07 23:07:22 +0100

    Rewrite re-exports for namespaced modules in index file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 export { addressRegister } from "~/configuration/address-register"
-export * as CockpitAssetPathForm from "~/functions/cockpit-asset-path-form"
-export * as CockpitDataAccess from "~/functions/cockpit-data-access"
-export * as CockpitDateConversion from "~/functions/cockpit-date-conversion"
-export * as QueryParameterForm from "~/functions/query-parameter-form"
 export { CockpitError } from "~/library/cockpit-error"
 export { Context, Environment } from "~/library/connection"
 export type { Address, APIToken, Protocol } from "~/library/connection"
@@ -13,3 +9,12 @@ export { CockpitImageRequest } from "~/models/cockpit-image-request"
 export { CockpitMetaData } from "~/models/cockpit-meta-data"
 export type { CockpitRequestOptions } from "~/models/cockpit-request-options"
 export type { AnyCockpitResponse, CockpitCollectionResponse, CockpitImageResponse, CockpitSingletonResponse } from "~/models/cockpit-response"
+import * as _CockpitAssetPathForm from "~/functions/cockpit-asset-path-form"
+import * as _CockpitDataAccess from "~/functions/cockpit-data-access"
+import * as _CockpitDateConversion from "~/functions/cockpit-date-conversion"
+import * as _QueryParameterForm from "~/functions/query-parameter-form"
+
+export const CockpitAssetPathForm = { ..._CockpitAssetPathForm }
+export const CockpitDataAccess = { ... _CockpitDataAccess }
+export const CockpitDateConversion = { ... _CockpitDateConversion }
+export const QueryParameterForm = { ... _QueryParameterForm }

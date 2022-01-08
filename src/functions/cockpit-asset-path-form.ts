07195ade8311df7b50eb26ea74db100227e5649c
August Saint Freytag <night@apricummedia.com>
2022-01-08 11:28:47 +0100

    Add mark-up descriptions to functions in asset path form module

--- src/functions/cockpit-asset-path-form.ts
+++ src/functions/cockpit-asset-path-form.ts
@@ -21,20 +21,26 @@ function pathPrefix(context: Context) {
 
 // Asset Path
 
+/** Returns the full path to an asset managed by Cockpit, including a prefix 
+ * suitable to be accessed from the given context (default: Client). */
 export function cockpitAsset(component: URLComponent, context: Context = Context.Client, prefix: boolean = true): URL {
 	return `${pathPrefix(context)}${cockpitAssetComponent(component)}`
 }
 
+/** Returns the local path component to an asset managed by Cockpit. */
 export function cockpitAssetComponent(component: URLComponent): URLComponent {
 	return `/storage/uploads${standardizedPathComponent(component)}`
 }
 
 // Image Path
 
+/** Returns the full path to an image managed by Cockpit, including a prefix 
+ * suitable to be accessed from the given context (default: Client). */
 export function cockpitImage(component: URLComponent, imageRequest: CockpitImageRequest, context: Context = Context.Client): URL {
 	return `${pathPrefix(context)}/${cockpitImageComponent(component, imageRequest)}`
 }
 
+/** Returns the local path component to an image managed by Cockpit. */
 export function cockpitImageComponent(component: URLComponent, imageRequest: CockpitImageRequest): URL {
 	const imageRequestOptions = imageRequest.options(standardizedPathComponent(component)) as ParameterDictionary
 	const joinedImageRequestOptions = joinedParameters(imageRequestOptions)
@@ -45,6 +51,7 @@ export function cockpitImageComponent(component: URLComponent, imageRequest: Coc
 
 // Utility
 
+/** Returns a standardized path component with a leading slash character. */
 function standardizedPathComponent(component: URLComponent): string {
 	if (component[0] === "/") {
 		return component
